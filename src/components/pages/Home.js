d7c45d7ff735398c1d6ae5205c8641f2bc842e6f
David Keim <dmk252@cornell.edu>
2023-05-09 16:54:49 -0400

    added cloud art

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { GridItem, Box, Text, Heading } from "@chakra-ui/react"
+import { GridItem, Box, Text, Heading, Image } from "@chakra-ui/react"
+import cloud from '../../assets/cloud-pic-big.png';
 
 // renders  code for resume section, including list of skills  and downloadable resume
 function Home({ currentPage, handlePageChange }) {
@@ -14,11 +15,19 @@ function Home({ currentPage, handlePageChange }) {
       alignItems="end"
       justifyContent="end"
     >
+      <Image
+        src={cloud}
+        alt="cloud sprite"
+        position="fixed"
+        right={-100}
+      >
+      </Image>
       <Box
         display="flex"
         flexDirection="row"
         justifyContent="center"
         alignItems="baseline"
+        zIndex="2"
       >
         <Heading
           fontSize="7xl"
@@ -42,6 +51,7 @@ function Home({ currentPage, handlePageChange }) {
         href="#portfolio"
         fontSize="2xl"
         onClick={() => handlePageChange('portfolio')}
+        zIndex="2"
       >
         View Projects
       </Text>
@@ -51,6 +61,7 @@ function Home({ currentPage, handlePageChange }) {
         as="a"
         fontSize="2xl"
         href="https://drive.google.com/file/d/1vI5HW-Z7eMg4IGimtN41F95RE3Zdgmit/view?usp=sharing"
+        zIndex="2"
       >
         Download Resume
       </Text>
