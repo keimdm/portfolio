4ef8bf247f32e817044fadc61ab3272f3c34065b
David Keim <dmk252@cornell.edu>
2023-05-09 09:50:36 -0400

    added home page formatting

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -11,8 +11,8 @@ function Home({ currentPage, handlePageChange }) {
       rowEnd={20}
       display="flex"
       flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
+      alignItems="end"
+      justifyContent="end"
     >
       <Box
         display="flex"
@@ -22,7 +22,7 @@ function Home({ currentPage, handlePageChange }) {
       >
         <Text
           fontFamily="serif"
-          fontSize="5xl"
+          fontSize="7xl"
           color="gray.500"
         >
           Hello, I'm&nbsp;&nbsp;
@@ -31,6 +31,7 @@ function Home({ currentPage, handlePageChange }) {
             fontFamily="serif"
             fontSize="9xl"
             color="blue.700"
+            fontWeight="bold"
           >
             David.
         </Text>
@@ -40,14 +41,16 @@ function Home({ currentPage, handlePageChange }) {
         mb={3}
         as="a"
         href="#portfolio"
+        fontSize="2xl"
         onClick={() => handlePageChange('portfolio')}
       >
         View Projects
       </Text>
       <Text
         color="gray.500"
-        mb={3}
+        mb={12}
         as="a"
+        fontSize="2xl"
         href="https://drive.google.com/file/d/1vI5HW-Z7eMg4IGimtN41F95RE3Zdgmit/view?usp=sharing"
       >
         Download Resume
