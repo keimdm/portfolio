c06337745f6d04e956d50220597af5fafeb2bc42
David Keim <dmk252@cornell.edu>
2023-05-10 17:14:20 -0400

    formatted about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import pic from "../../assets/profile-pic.png";
-import { GridItem } from "@chakra-ui/react"
+import pic from "../../assets/new-profile-pic.png";
+import { GridItem, Heading, Image, Text, Box } from "@chakra-ui/react"
 
 // returns about section with picture and short bio
 function About() {
@@ -15,15 +15,75 @@ function About() {
         alignItems="center"
         justifyContent="center"
         zIndex="2"
+        pl={24}
       >
-        <h3>About</h3>
-        <img className="w-50" src={pic} alt="Profile"></img>
-        <p className="text-center">
-          Full stack web developer with background in chemical engineering and management consulting.
-        </p>
-        <p className="text-center">
-          Interested in designing games, music, and good food.
-        </p>
+        <Heading
+          fontSize="7xl"
+          w="352px"
+          fontWeight="700"
+          color="blue.700"
+          textAlign="center"
+          zIndex="2"
+          alignSelf="start"
+
+        >
+          About Me
+        </Heading>
+        <Box
+          display="flex"
+          flexDirection="row"
+        >
+          <Box
+            borderRadius='10px'
+            backgroundColor="white"
+            px={5}
+            py={5}
+            maxWidth="50%"
+          >
+            <Image
+              borderRadius='10px'
+              src={pic}
+              alt="Profile Picture">
+            </Image>
+          </Box>
+          <Box
+            display="flex"
+            flexDirection="column"
+            maxWidth="50%"
+            justifyContent="space-between"
+            px={5}
+            py={5}
+          >
+            <Text
+              color="gray.500"
+              pb={3}
+              fontSize="2xl"
+              zIndex="2"
+            >
+              Full stack web developer with background in chemical engineering and management consulting.<br></br><br></br>Interested in designing games, music, and good food.
+            </Text>
+            <Box
+              px={6}
+              mb={6}
+              as="a"
+              fontSize="2xl"
+              href="https://drive.google.com/file/d/1vI5HW-Z7eMg4IGimtN41F95RE3Zdgmit/view?usp=sharing"
+              zIndex="2"
+              borderColor="gray.500"
+              borderWidth="2px"
+              color="gray.500"
+              borderRadius="10px"
+              alignSelf="center"
+              _hover={{
+                backgroundColor: "blue.700",
+                borderColor: "blue.700",
+                color: "white"
+              }}
+            >
+              Download Resume
+            </Box>
+          </Box>
+        </Box>
       </GridItem>
   );
 }
